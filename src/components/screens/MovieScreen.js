efce1a77bca45349f9f9b05930ac7673a276a9e3
Juan sebastian <juans.cardenasg@autonoma.edu.co>
2023-09-18 16:06:46 -0500

    first commit

--- src/components/screens/MovieScreen.js
+++ src/components/screens/MovieScreen.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
-  Text,
   ActivityIndicator,
   FlatList,
 } from "react-native";
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, Box, Badge, Text } from "native-base";
+import { useAxios } from "../../utils/useAxios";
 
-import {useAxios} from "../../utils/useAxios";
+import { Stack, TextInput, IconButton } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+
 
 const MovieScreen = () => {
+  const [searchQuery, setSearchQuery] = useState(""); // Estado para almacenar la consulta de búsqueda
+
   const { data, loading, error, handleCancelRequest } = useAxios(
-    "https://fakestoreapi.com/products/"
+    `https://apis-backend-dm.up.railway.app/api/v1/peliculas/${searchQuery}` // Usar la consulta como parte de la URL
   );
+
+  /* const handleSearch = () => {
+    // Llamar a la API con el valor de búsqueda
+    // Aquí puedes realizar la llamada a la API nuevamente con el nuevo valor de búsqueda
+  }; */
 
   if (loading) {
     return (
@@ -38,15 +47,51 @@ const MovieScreen = () => {
 
   return (
     <NativeBaseProvider>
+      <View style={styles.searchContainer}>
+        <TextInput
+          label="Pelicula"
+          variant="standard"
+          placeholder="Buscar película..."
+          value={searchQuery}
+          style={styles.input}
+          onChangeText={(text) => setSearchQuery(text)} // Actualizar el estado
+        />
+        {/* <IconButton
+          style={styles.searchButton}
+          icon={(props) => <Icon name="magnify" {...props} />}
+          onPress={handleSearch}
+        /> */}
+      </View>
+
       <FlatList
         data={data}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item) => item.poster_path}
         renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
-            <Text>{item.title}</Text>
-            <Text>{item.price}</Text>
-            <Text>{item.category}</Text>
-          </View>
+          /* card */
+          <Box
+            bg="white"
+            shadow={2}
+            rounded="lg"
+            maxWidth="90%"
+            width="90%"
+            mx="auto"
+            mt={5}
+          >
+            {/* popularity */}
+            <Badge style={styles.popularityBadge}>
+              <Text color="#fff" fontSize="xs">
+                {item.popularity}
+              </Text>
+            </Badge>
+            <Box px={4} py={2} style={styles.textContainer}>
+              <Text fontWeight="bold" color="#6200ee">
+                {item.original_title}
+              </Text>
+              <Text fontSize="sm" mt={1}>
+                {item.release_date}
+              </Text>
+            </Box>
+          </Box>
         )}
       />
     </NativeBaseProvider>
@@ -64,10 +109,39 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  itemContainer: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+  popularityBadge: {
+    position: "absolute",
+    top: 2,
+    right: 2,
+    marginTop: 8,
+    marginRight: 5,
+    backgroundColor: "#6200ee",
+    color: "#fff",
+    padding: 2,
+    borderRadius: 999, // Un valor alto para hacer que el Badge sea circular
+  },
+  textContainer: {
+    color: "#fff",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  input: {
+    marginVertical: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+  },
+  searchButton: {
+    marginLeft: 10,
+    marginRight: 20,
   },
 });
 
